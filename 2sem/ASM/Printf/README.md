8e8bc5027b1a92badecc6878a478a1271f783b91
exesaLigno <cfg888powerfull@gmail.com>
2020-04-17 20:14:24 +0300

    forout_nomem removed Added testing keys to main.c

--- 2sem/ASM/Printf/README.md
+++ 2sem/ASM/Printf/README.md
@@ -6,18 +6,18 @@ forout(const char* format_string, parameter1, parameter2, ...);
 ```
 Function returning count of printed symbols
 # Assembly
-To compile programm write
-```
-make
-```
-To delete all compiled files type
-```
-make clean
-```
+To compile programm write `make`
+To delete all compiled files type `make clean`
+
+# Testing
+Testing programm support two types of usage - beautiful and clear. In beautiful mode (run with `--beautiful` key) output highlighted.
+In clear mode (run with `--clear` key) output not highlighted. It's useful for testing printed symbols counter.
+
 # Time managment
 ```
 7 h - Reading mans
 2 h - Designing programm structure
 8 h - Writing programm
 6 days - panic about coronavirus
+4 h - Rewriting with less syscalls
 ```
--- 2sem/ASM/Printf/main.c
+++ 2sem/ASM/Printf/main.c
@@ -1,32 +1,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int forout_nomem(const char*, ...);
 int forout(const char*, ...);
 
-int main()
+void beautiful_test();
+void clear_test();
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)
+		beautiful_test();
+	else if (!strcmp(argv[1], "--clear"))
+		clear_test();
+	else if (!strcmp(argv[1], "--beautiful"))
+		beautiful_test();
+	else
+		printf("Unknown parameter!\n");
+		
+	return 0;
+}
+
+
+void beautiful_test()
 {
 	char string1[10] = "Vodka";
 	char string2[10] = "people";
 	int test_number = 0xBEDA;
 	int test = 0;
-	
-	printf("\x1b[1;32m----- Old forout test -----\x1b[0m\n\n");
-	test = forout_nomem("\x1b[1;32m%s\x1b[0m connecting %s%c\n", string1, string2, '!');
-	forout_nomem("---Printed %d symbols---\n\n", test);
-	test = forout_nomem("%d in other cs:\n\t%b (binary);\n\t%q (quaternary);\n\t%o (octal);\n\t%h (heximal)\n", test_number, test_number, test_number, test_number, test_number);
-	forout_nomem("---Printed %d symbols---\n\n", test);
-	forout_nomem("Many parameters test: %d %d %d %d %d %d %d %d %d %d %d %d\n\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-	printf("\x1b[1;31m----- Old forout test END -----\x1b[0m\n\n");
 
-	printf("\x1b[1;32m----- NEW forout test -----\x1b[0m\n\n");
-	test = forout("\x1b[1;32m%s\x1b[0m connecting %s%c\n", string1, string2, '!');
+	printf("\x1b[1;32m----- forout test -----\x1b[0m\n\n");
+	
+	forout("\x1b[5;32m%s connecting %s%c\x1b[0m\n\n", string1, string2, '!');
+	forout("\x1b[3m%d\x1b[0m in other cs:\n\t\x1b[3m%b\x1b[0m (binary);\n\t\x1b[3m%q\x1b[0m (quaternary);\n\t\x1b[3m%o\x1b[0m (octal);\n\t\x1b[3m%h\x1b[0m (heximal)\n\n", test_number, test_number, test_number, test_number, test_number);
+	forout("Many parameters test: \x1b[3m%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\x1b[0m\n\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+	
+	printf("\x1b[1;31m----- forout test END -----\x1b[0m\n\n");
+}
+
+void clear_test()
+{
+	char string1[10] = "Vodka";
+	char string2[10] = "people";
+	int test_number = 0xBEDA;
+	int test = 0;
+
+	printf("\x1b[1;32m----- forout test -----\x1b[0m\n\n");
+	
+	test = forout("%s connecting %s%c\n", string1, string2, '!');
 	forout("---Printed %d symbols---\n\n", test);
+	
 	test = forout("%d in other cs:\n\t%b (binary);\n\t%q (quaternary);\n\t%o (octal);\n\t%h (heximal)\n", test_number, test_number, test_number, test_number, test_number);
 	forout("---Printed %d symbols---\n\n", test);
-	test = forout("Many parameters test: %d %d %d %d %d %d %d %d %d %d %d %d\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-	forout("---Printed %d symbols---\n\n", test);
-	printf("\x1b[1;31m----- NEW forout test END -----\x1b[0m\n\n");
 	
-	return 0;
+	test = forout("Many parameters test: %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+	forout("---Printed %d symbols---\n\n", test);
+	
+	printf("\x1b[1;31m----- forout test END -----\x1b[0m\n\n");
 }
+
+
+
