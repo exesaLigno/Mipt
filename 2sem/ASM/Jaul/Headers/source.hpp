998c075944c7aa8cdd4fd74ca62dd54a180b489b
exesaLigno <cfg888powerfull@gmail.com>
2020-06-02 08:55:00 +0300

    Source class accomplished

--- 2sem/ASM/Jaul/Headers/source.hpp
+++ 2sem/ASM/Jaul/Headers/source.hpp
@@ -1,3 +1,5 @@
+#include <cstring>
+
 class Source
 {
   protected:
@@ -6,26 +8,38 @@ class Source
 	char* text;
 	long long int text_length;
 	
+  private:
+	char* text_pointer;
+	
   public:
-	Source(const char* name, short int source_type);
+	Source(const char* name);
 	~Source();
 	
-	int open();
-	Source* getDefinitions();
+	short int open();
+	short int getType(const char* name);
+	
+	int getDefCount();
+	Source* getDefinition();
 	int substitute(Source& source);
+	
+	void rewind();
 	
 	enum SourceTypes
 	{
-		JAUL_SOURCE,
-		NASM_SOURCE,
-		OBJ_SOURCE,
-		DEFINITION
+		JAUL_SOURCE = 1,		// .jaul
+		NASM_SOURCE,			// .s
+		OBJ_SOURCE,				// no extension
+		VIRTUAL_EXECUTABLE,		// .jaulv
+		DEFINITION,				// fileless type
+		ERRTYPE
 	};
 	
 	enum Errors
 	{
+		OK = 0,
 		OPENNING_PROHIBITED = -1,
 		FILE_NOT_EXIST = -2,
-		UNSUPPORTED_FILE_EXTENSION = -3
+		UNSUPPORTED_FILE_EXTENSION = -3,
+		NOT_SUBSTITUTABLE_TYPE = -4
 	};
 }
--- 2sem/ASM/Jaul/Source/source.cpp
+++ 2sem/ASM/Jaul/Source/source.cpp
@@ -1,3 +1,6 @@
+#include "../Headers/source.hpp"
+
+
 Source::Source(const char* name)
 {
 	this -> name = new char[strlen(name) + 1]{0};
@@ -6,34 +9,49 @@ Source::Source(const char* name)
 	this -> text = NULL;
 	this -> text_length = NULL;
 	
+	this -> text_pointer = NULL;
+	
 	this -> source_type = NULL;
 }
 
 Source::~Source()
 {
-	if (this -> name)
+	if (this -> name != NULL)
 		delete[] this -> name;
 		
-	if (this -> text)
+	if (this -> text != NULL)
 		delete[] this -> text;
 }
 
-int open()
+short int Source::open()
 {
-	if (this -> source_type != JAUL_SOURCE or this -> source_type != NASM_SOURCE or this -> source_type != OBJ_SOURCE)
+	if (this -> source_type == DEFINITION)
 		return OPENNING_PROHIBITED;
-		
-	if (0) // Сравнение расширения с текущим типом и возврат кода ошибки
-		return UNSUPPORTED_FILE_EXTENSION;
-		
-	FILE* source_file = fopen(this -> name, "r");
+
+	short int type = getType(this -> name);
 	
-	// Поиск в стандартной директории библиотек
+	if (type == ERRTYPE)
+		return UNSUPPORTED_FILE_EXTENSION;
+	
+	else
+		this -> source_type = type;
+		
+	FILE* source_file = fopen(this -> name, "r");	// Trying to open file
+	
+	if (not source_file)	// Trying to find file in std library path
+	{
+		// TODO config reading from Jaul.conf
+		char* std_name = new char[strlen(this -> name) + strlen(".std/") + 1]{0};
+		strcpy(std_name, ".std/");
+		strcat(std_name, this -> name);
+		source_file = fopen(std_name, "r");
+		delete[] std_name;
+	}
 	
 	if (not source_file)
 	{
-		printf("\x1b[1;31mError\x1b[0m: file \x1b[1m\"%s\"\x1b[0m not exist.\n", source_path);
-		exit(FILE_NOT_EXIST);
+		printf("\x1b[1;31mError\x1b[0m: file \x1b[1m\"%s\"\x1b[0m not exist.\n", this -> name);
+		return FILE_NOT_EXIST;
 	}
 	
 	fseek(source_file, 0, SEEK_END);
@@ -41,9 +59,108 @@ int open()
 	rewind(source_file);
 
 	this -> text = new char[this -> text_length + 1]{0};
+	
+	this -> text_pointer = this -> text;
 
 	this -> text_length = fread(this -> text, sizeof(char), this -> text_length, source_file);
 	
 	fclose(source_file);
+	
+	return OK;
 }
+
+short int Source::getType(const char* name)
+{
+	char* extension = strchr(name, '.');
+	
+	if (extension == nullptr)
+		return OBJ_SOURCE;
+		
+	else
+	{
+		if (!strcmp(extension, ".jaul"))
+			return JAUL_SOURCE;
+			
+		else if (!strcmp(extension, ".s"))
+			return NASM_SOURCE;
+			
+		else if (!strcmp(extension, ".jaulv"))
+			return VIRTUAL_EXECUTABLE;
+			
+		else
+			return ERRTYPE;
+	}
+}
+
+int getDefCount()
+{
+	int def_count = 0
+
+	char* finded = strstr(this -> text, "define ");
+	
+	while (finded != nullptr)
+	{
+		def_count++;
+		finded = strstr(++finded, "define ");
+	}
+	
+	return def_count;
+}
+
+Source* Source::getDefinition()
+{
+	char* text_pointer = strstr(this -> text_pointer, "define ");
+}
+
+int Source::substitute(const Source& source)
+{
+	if (source.source_type != DEFINITION)
+		return NOT_SUBSTITUTABLE_TYPE;
+		
+	int name_length = strlen(source.name);
+	int substitution_length = source.text_length;
+		
+	long long int new_text_length = this -> text_length;
+	
+	char* finded = strstr(this -> text, source.name);
+	
+	while (finded != nullptr)
+	{
+		new_text_length = new_text_length - name_length + substitution_length;
+		finded = strstr(++finded, source.name);
+	}
+	
+	char* new_text = new char[new_text_length + 1]{0};
+	char* _new_text = new_text;
+	char* _text = this -> text;
+	
+	finded = strstr(this -> text, source.name);
+	
+	while (finded != nullptr)
+	{
+		strncat(_new_text, _text, finded - _text);
+		_new_text += finded - _text;
+		_text = finded + name_length;
+		
+		strcat(_new_text, source.text);
+		_new_text += substitution_length;
+		
+		finded = strstr(finded, source.name);
+	}
+	
+	delete[] this -> text;
+	
+	this -> text = new_text;
+	this -> text_length = new_text_length;
+	
+	return OK;
+}
+
+void Source::rewind()
+{
+	this -> text_pointer = this -> text;
+}
+
+
+
 
