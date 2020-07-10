d175260ad9e639cced3b6e37bd869b0d35985d97
exesaLigno <cfg888powerfull@gmail.com>
2020-07-10 14:56:42 +0300

    Binary class base created

--- 2sem/ASM/Jaul/Headers/binary.hpp
+++ 2sem/ASM/Jaul/Headers/binary.hpp
@@ -1,8 +1,72 @@
 #pragma once
 
+#include <cstring>
+#include "ast.hpp"
+#include "source.hpp"
+
 class Binary
 {
   public:
+  	class Token;
+	class Header;
+	
+	Source* source = nullptr;
+	
+	Token* start = nullptr;
+	Token* end = nullptr;
+	
 	Binary();
+	Binary(Source* source);
 	~Binary();
+	
+	int compileAst();		// push back and generate byte code
+	int compileNasm();		// push back and generate byte code
+	int importObj();		// manual - taking name of func, byte code of func and creating new token
+	
+	int pushBack(const char* nasm_code);
+	
+	int exportNasm();
+	int exportObj();
+	int exportExecutable();
+	int exportVirtualExecutable();
+};
+
+
+class Binary::Token
+{
+	friend Binary;
+	
+  protected:
+	char* label = nullptr;
+	short int label_type = 0;
+	
+	char* nasm_code = nullptr;
+	char* byte_code = nullptr;
+	int byte_code_length = 0;
+	long long int first_byte_position = 0;
+	
+	Token* prev = nullptr;
+	Token* next = nullptr;
+	
+	enum LabelTypes{EMPTY, LOCAL, GLOBAL, FUNCTION};
+	
+  public:
+	Token();
+	Token(const char* name, const char* nasm_text);
+	int setLabel(const char* name, int name_length);
+	int setBytes(const char* bytes, long int bytes_count);
+	~Token();
+	
+	int toNasm();	// if byte code given, generating NASM code
+	int toByte();	// the same, but from NASM to byte code
+};
+
+
+class Binary::Header
+{
+	friend Binary;
+  
+  protected:
+	Header();
+	~Header();
 };
--- 2sem/ASM/Jaul/Source/binary.cpp
+++ 2sem/ASM/Jaul/Source/binary.cpp
@@ -3,5 +3,109 @@
 Binary::Binary()
 {}
 
+Binary::Binary(Source* source)
+{
+	this -> source = source;
+}
+
 Binary::~Binary()
+{}
+
+int Binary::compileAst()
+{
+	return 0;
+}
+
+int Binary::compileNasm()
+{
+	return 0;
+}
+
+int Binary::importObj()
+{
+	return 0;
+}
+
+int Binary::pushBack(const char* nasm_code)
+{
+	
+	
+	return 0;
+}
+
+int Binary::exportNasm()
+{
+	return 0;
+}
+
+int Binary::exportObj()
+{
+	return 0;
+}
+
+int Binary::exportExecutable()
+{
+	return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+Binary::Token::Token()
+{}
+
+Binary::Token::Token(const char* name, const char* nasm_text)
+{
+	this -> label = new char[strlen(name) + 1]{0};
+	strcpy(this -> label, name);
+	
+	this -> nasm_code = new char[strlen(name) + 1]{0};
+	strcpy(this -> nasm_code, nasm_text);
+}
+
+Binary::Token::~Token()
+{
+	if (this -> label != nullptr)
+		delete[] this -> label;
+		
+	if (this -> nasm_code != nullptr)
+		delete[] this -> nasm_code;
+		
+	if (this -> byte_code != nullptr)
+		delete[] this -> byte_code;
+}
+
+int Binary::Token::setLabel(const char* name, int name_length)
+{
+	this -> label = new char[name_length + 1]{0};
+	strncpy(this -> label, name, name_length);
+	return 0;
+}
+
+int Binary::Token::setBytes(const char* bytes, long int bytes_count)
+{
+	this -> byte_code = new char[bytes_count + 1]{0};
+	strncpy(this -> byte_code, bytes, bytes_count);
+	return 0;
+}
+
+int Binary::Token::toNasm()
+{
+	return 0;
+}
+
+int Binary::Token::toByte()
+{
+	return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+Binary::Header::Header()
+{}
+
+Binary::Header::~Header()
 {}
