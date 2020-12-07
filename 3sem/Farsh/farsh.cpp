23b5047986bd7e862ee049ef47dc666a30228586
exesaLigno <cfg888powerfull@gmail.com>
2020-12-08 01:25:34 +0300

    farSH updated - now it works in a simplest way - just starting program with parameters (piping is not implemented)

--- 3sem/Farsh/farsh.cpp
+++ 3sem/Farsh/farsh.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <cstring>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 class Shell
@@ -11,12 +13,12 @@ class Shell
 	char* current_directory_str = nullptr;
 	char* current_user_str = nullptr;
 	char* current_pc_name = nullptr;
-	
+
   public:
 	Shell();
 	~Shell();
 	void printGreeting();
-	void executeCommand(const char* command);
+	void executeCommand(char* command);
 	void polling();
 };
 
@@ -42,19 +44,19 @@ void Shell::printGreeting()
 {
 	char greeting[1000] = {};
 	char* full_path = this -> current_directory_str;
-	
+
 	char home_path[1000] = {};
 	strcat(home_path, "/home/");
 	strcat(home_path, this -> current_user_str);
-	
+
 	if (!strncmp(this -> current_directory_str, home_path, strlen(home_path)))
 	{
 		strcat(greeting, "~");
 		full_path += + strlen(home_path);
 	}
-	
+
 	//strcat(greeting, full_path);
-	
+
 	while (true)
 	{
 		if (*full_path == '/')
@@ -62,25 +64,25 @@ void Shell::printGreeting()
 			strcat(greeting, "/");
 			full_path++;
 		}
-		
+
 		char* next_slash = strchr(full_path, '/');
-		
+
 		if (next_slash != nullptr)
 		{
 			strncat(greeting, full_path, 1);
 			full_path = next_slash;
 		}
-		
+
 		else
 		{
 			strcat(greeting, full_path);
 			break;
 		}
 	}
-	
-	
+
+
 	printf("\x1b[34m%s\x1b[0m@%s \x1b[32m%s\x1b[0m> ", this -> current_user_str, this -> current_pc_name, greeting);
-	
+
 	//printf("\x1b[33m%s\x1b[0m \x1b[32m%s\x1b[0m\n", this -> current_user_str, this -> current_directory_str);
 }
 
@@ -91,20 +93,23 @@ void Shell::polling()
 	{
 		this -> printGreeting();
 		char command[1000] = {};
-		
+
 		int counter = 0;
 		char symbol = 0;
-		
+
 		while (true)
 		{
 			symbol = getchar();
-			
+
 			if (symbol == '\n')
 				break;
-				
+
 			else if (symbol == EOF)
+			{
+				printf("exiting...\n");
 				return;
-			
+			}
+
 // 			TODO Arrows buttons processing is not implemented yet
 // 			else if (symbol == 27)
 // 			{
@@ -113,26 +118,84 @@ void Shell::polling()
 // 				//printf("%d\n", cmd);
 // 				//printf("\b\b\b");
 // 			}
-				
+
 			else
 				command[counter++] = symbol;
 		}
-		
+
 		this -> executeCommand(command);
 	}
 }
 
 
-void Shell::executeCommand(const char* command)
+void Shell::executeCommand(char* command)
 {
-	printf("Executing command «%s»\n", command);
+	int word_count = 0;
+	
+	int counter = 0;
+	
+	while (command[counter] != '\0')
+	{
+		while (command[counter] == ' ')
+			counter++;
+		
+		if (command[counter] != '\0' and command[counter] != ' ')
+			word_count++;
+		
+		while (command[counter] != ' ' and command[counter] != '\0')
+			counter++;
+	}
+	
+	char* command_parsed[word_count + 1];
+		
+	word_count = 0;
+	
+	counter = 0;
+	
+	while (command[counter] != '\0')
+	{
+		while (command[counter] == ' ')
+		{
+			command[counter] = '\0';
+			counter++;
+		}
+		
+		if (command[counter] != '\0' and command[counter] != ' ')
+		{
+			command_parsed[word_count] = command + counter;
+			word_count++;
+		}
+		
+		while (command[counter] != ' ' and command[counter] != '\0')
+			counter++;
+	}
+	
+	command_parsed[word_count] = NULL;
+	
+	pid_t child = fork();
+	
+	int wstatus = 0;
+	
+	if (!child)
+	{
+		execvp(command_parsed[0], command_parsed);
+		perror("something gone wrong");
+		exit(1);
+	}
+	
+	else
+	{
+		wait(&wstatus);
+		if (wstatus != 0)
+			printf("can't execute command «%s»\n", command);
+			
+	}
 }
 
 
 int main()
 {
 	Shell farSH;
-	//farSH.getPathRepr();
 	farSH.polling();
 	return 0;
 }
