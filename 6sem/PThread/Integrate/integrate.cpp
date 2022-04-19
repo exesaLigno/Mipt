5a8fb5cf724ea1b7f639a9ac2ceb067d3e3c7d2f
Vadim Karcev <cfg888powerfull@gmail.com>
2022-04-20 00:47:50 +0300

    integration added

--- 6sem/PThread/Integrate/integrate.cpp
+++ 6sem/PThread/Integrate/integrate.cpp
@@ -22,6 +22,17 @@ double integrate(double f(double), double start, double stop, double dx);
 
 int main(int argc, char** argv)
 {
+  if (argc < 3)
+  {
+    printf("Usage: ./integrate [threads_count] [accuracy]");
+    return 1;
+  }
+
+  int threads_count = atoi(argv[1]);
+  double accuracy = atof(argv[2]);
+
+  printf("Calculating integral on %d threads with %lg accuracy\n", threads_count, accuracy);
+
   double start = 1.2, stop = 9.1, dx = 0.001;
   printf("Result: %lg\n", integrate(quad, start, stop, dx));
   return 0;
