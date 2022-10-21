5a2bdba88241dae46869a25751ea9a5d967c8906
Vadim Karcev <cfg888powerfull@gmail.com>
2022-10-21 19:04:41 +0300

    Checked

---
+++ 7sem/OpenMP/AdditionalTasks/FFT/fft_helper.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+def CheckAmountCorrectness(amount):
+    t = 1
+    while t < amount:
+        t *= 2
+    return t == amount
+
+def GenerateComplexSignal(amount):
+    f = lambda x: 10 * (np.sin(2 * np.pi * x) + np.sin(15 * np.pi * x) + np.sin(100000 * np.pi * x))
+
+    x_band = np.linspace(0, 1, amount)
+    f_band = f(x_band)
+
+    return f_band, np.fft.fft(f_band)
+
+def DrawFFTPlot(filename):
+    file = open(filename, 'r')
+    values = list(map(float, file.read().split()))
+    file.close()
+
+    plt.figure(figsize=[12, 5])
+    plt.title(f'{filename}')
+    plt.plot(values)
+    plt.savefig(f'{filename[:-4]}_plot.png')
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3 and sys.argv[1] == 'generate':
+        amount = int(sys.argv[2])
+        if not CheckAmountCorrectness(amount):
+            print(f'{amount} is incorrect value for amount, use powers of 2')
+            sys.exit()
+
+        f_band, spectre = GenerateComplexSignal(int(amount))
+
+        file = open(f'signals/{amount}_signal.txt', 'w')
+        for f in f_band:
+            file.write(f'{f:.4f} ')
+        file.close()
+
+        file = open(f'signals/{amount}_reference_spectre.txt', 'w')
+        for s in spectre:
+            file.write(f'{np.abs(s):.4f} ')
+        file.close()
+
+    elif len(sys.argv) == 3 and sys.argv[1] == 'plot':
+        DrawFFTPlot(sys.argv[2])
+
--- 7sem/OpenMP/AdditionalTasks/FFT/main.cpp
+++ 7sem/OpenMP/AdditionalTasks/FFT/main.cpp
@@ -1,14 +1,25 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include "lib/FFT.cpp"
 #include "lib/clocking.cpp"
 
 void show_results();
-
+void read_signal(double* signal, long signal_length);
+void write_spectre(cpl* spectre, long signal_length);
+void compare_times(long amount);
+void calculate(long amount);
 
 int main(int argc, char** argv)
 {
-    show_results();
+    if (argc > 1 and !strcmp(argv[1], "show"))
+        show_results();
+    
+    else if (argc > 2 and !strcmp(argv[1], "compare"))
+        compare_times(atol(argv[2]));
+
+    else if (argc > 2 and !strcmp(argv[1], "calc"))
+        calculate(atol(argv[2]));
 
     return 0;
 }
@@ -31,4 +42,56 @@ void show_results()
     for (int i = 0; i < 8; i++)
         spectrum[i].print("; ");
     printf("\n");
+}
+
+void read_signal(double* signal, long signal_length)
+{
+    char filename[100] = {};
+    sprintf(filename, "signals/%ld_signal.txt", signal_length);
+    FILE* signal_file = fopen(filename, "r");
+    int n = 0;
+    double readed = 0;
+    while (fscanf(signal_file, "%lf", &(signal[n])) == 1)
+        n++;
+    fclose(signal_file);
+}
+
+void write_spectre(cpl* spectre, long signal_length)
+{
+    char filename[100] = {};
+    sprintf(filename, "signals/%ld_spectre.txt", signal_length);
+    FILE* spectre_file = fopen(filename, "w");
+    for (int i = 0; i < signal_length; i++)
+        fprintf(spectre_file, "%.4lf ", spectre[i].abs());
+    fclose(spectre_file);
+}
+
+void compare_times(long amount)
+{
+    double* signal = new double[amount];
+    cpl* spectre = new cpl[amount];
+
+    read_signal(signal, amount);
+
+    auto start_time = clk();
+    FFT(signal, amount, spectre, false);
+    auto linear_time = clk() - start_time;
+
+    start_time = clk();
+    FFT(signal, amount, spectre, true);
+    auto parallel_time = clk() - start_time;
+
+    printf("For %ld points: \n Linear = %ld mks \n Parallel = %ld mks\n", amount, linear_time, parallel_time);
+}
+
+void calculate(long amount)
+{
+    double* signal = new double[amount];
+    cpl* spectre = new cpl[amount];
+
+    read_signal(signal, amount);
+
+    FFT(signal, amount, spectre);
+
+    write_spectre(spectre, amount);
 }
